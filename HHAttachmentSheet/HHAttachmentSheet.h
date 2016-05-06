7c4b3dab1b4fd3d6b383d9a3dc00a39be3871d9c
herui <heruicross@163.com>
2016-05-06 17:38:50 +0800

    remove framework version

--- HHAttachmentSheet/HHAttachmentSheet.h
+++ HHAttachmentSheet/HHAttachmentSheet.h
@@ -8,15 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-//! Project version number for HHAttachmentSheet.
-FOUNDATION_EXPORT double HHAttachmentSheetVersionNumber;
-
-//! Project version string for HHAttachmentSheet.
-FOUNDATION_EXPORT const unsigned char HHAttachmentSheetVersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <HHAttachmentSheet/PublicHeader.h>
- 
-// #import "HHAttachmentSheetView.h"
 
 #import "HHAttachmentSheetView.h"
 #import "HHAttachmentSheetItemView.h"
